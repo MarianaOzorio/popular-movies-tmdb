9bca2b0f76513fb6cfdfc424af4dea42b5f6c9bc
marianaozorio <mariana.athayde@gmail.com>
2023-02-16 19:12:08 -0300

    Create router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Details from './pages/Details/Details';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/details/:id" element={<Details/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
